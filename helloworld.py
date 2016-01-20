20922ae250f4af37c718d1978b038fcf5af9de6d
Hoa Nguyen <Hnguyen124@mail.valenciacollege.edu>
2016-01-20 10:11:33 -0500

    assignment

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,24 @@
-# replace the contents of this comment with your full name
+# Hoa Nguyen
 
-# write a program that:
-# 1. greets the user in English
-# 2. asks the user to choose from 1 of 3 spoken languages (pick your favorite languages!) 
-# 3. displays the greeting in the chosen language
-# 4. exits
+# This program says hello world and ask for my language.
+print('Hello world!')
+print('Choose a language and I will greet you in that languge!') # ask for their language
+print('1. Vietnamese')
+print('2. Portuguese')
+print('3. Turkish')
+language = input()
+if language == 'Vietnamese':
+    print('Chao the gioi!')
+if language == 'Portuguese':
+    print('Ola Mundo!')
+if language == 'Turkish':
+    print('Selam Dunya!')
+import sys
 
-# make sure that your code contains comments explaining your logic!
+while True:
+    print('Type exit to exit.')
+    response = input()
+    if response == 'exit':
+        sys.exit()
+    print('You typed ' + response + '.')
 
